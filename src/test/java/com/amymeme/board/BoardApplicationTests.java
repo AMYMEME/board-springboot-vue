b23c45038d8776aafd95808cb3b93aa1674912cd
amy choi <cgc8016@ewhain.net>
2020-09-16 17:16:16 +0900

    initial commit

---
+++ src/test/java/com/amymeme/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amymeme.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
